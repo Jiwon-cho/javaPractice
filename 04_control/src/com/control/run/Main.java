0e9a9c31e1f77e466f8bf04892b4095d3561b60b
chojiwon <gwon188@gmail.com>
2021-01-25 22:49:22 +0900

    Second resitory[master]

--- 04_control/src/com/control/run/Main.java
+++ 04_control/src/com/control/run/Main.java
@@ -5,15 +5,16 @@ import com.repeat.controller.REpeatController;
 public class Main {
 	public static void main(String[] args) {
 //		new ConditionController().switchTest();
-		new REpeatController().basicFor();
+//		new REpeatController().basicFor();
+//		new REpeatController().Student();
+//		new REpeatController().ExtraFor();
+//		new REpeatController().Cal();		
+		new REpeatController().WhileTest();	
+//		new REpeatController().forInfor();
 		
 		
 		
 		
-		
-		
-		
-		
-	}
+		}
 
 }
