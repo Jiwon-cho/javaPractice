72f5d771fab78300a06c632d22ec73b20adf56ba
chojiwon <gwon188@gmail.com>
2021-03-08 22:31:56 +0900

    Movie

--- movie/src/movie/model/vo/Test2.java
+++ movie/src/movie/model/vo/Test2.java
@@ -92,12 +92,7 @@ public static void main(String[] args) {
 	}
 		System.out.println();
 	}
-		//너무 어렵다. 나는 어떻게 데이터를 정재해야할까
-			//시간이 너무 없다 머리 빠개질꺼같다.
-		//어떻게 하면 CATEGORY 를 넣을 수 있을까 머이아프네...
-		//Movie().mainmenu();	
-	//50까지 해서 해결.
-	//정처기 합격!
+		
 
 		}
 
