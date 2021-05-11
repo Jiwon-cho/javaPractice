412cecea21d81d90803ee9ce04a67f084e5e2b70
chojiwon <gwon188@gmail.com>
2021-05-11 23:57:34 +0900

    JAVA

--- TestEx/src/com/practice2/aa.java
+++ TestEx/src/com/practice2/aa.java
@@ -1,12 +1,18 @@
 package com.practice2;
 
 public class aa {
+	private sss s;
 
-	public static void main(String[] args) {
+	public static void sksk() {
 		// TODO Auto-generated method stub
 		double d=8.6;
 		int a=(int)d;
 		System.out.println(a);
+		s.M();
+	}
+	
+	public static void main(String[] args) {
+		sksk();
 	}
 
 }
---
+++ TestEx/src/com/practice2/sss.java
@@ -0,0 +1,7 @@
+package com.practice2;
+
+public abstract class  sss {
+	public void M() {
+		System.out.println("slslls");
+	}
+}
